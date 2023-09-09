abcc6f2ac95bc12c382b27df3c8a17ce53774f54
RahimKorbo <rkorbo@gmail.com>
2023-09-09 10:32:41 +0530

    master: first commit setup

---
+++ src/main/java/com/rk/bloodlab/BloodlabApplication.java
@@ -0,0 +1,13 @@
+package com.rk.bloodlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rk/bloodlab/BloodlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.bloodlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
